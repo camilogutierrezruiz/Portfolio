5a487e8091ff6305aee0b71d7797acbf673ac4e1
camilogutierrezruiz <camilogutierrez593@hotmail.com>
2022-02-18 17:49:01 -0500

    Finalizacion de casi todo el proyecto, haciendo falta portafolio y desktop version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const menuButton = document.querySelector('.nav__button');
-const closeButton = document.querySelector('.close__menu');
+const closeButton = document.querySelector('.nav__button');
 const navMenu = document.querySelector('.menu__nav');
 
 menuButton.addEventListener('click', function () {
